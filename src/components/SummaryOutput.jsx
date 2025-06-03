c22cd21ff0c4fb385de6b2cf261da25dd0bb6bf5
AAA RENTAL LLP <aaarentalllp@Yashs-MacBook-Pro.local>
2025-06-03 12:15:45 +0530

    minor fixes

--- src/components/SummaryOutput.jsx
+++ src/components/SummaryOutput.jsx
@@ -5,8 +5,11 @@ export default function SummaryOutput({ summary }) {
   const { darkMode } = useTheme();
   return (
     <div className={`mt-6 ${darkMode ? 'custom-dark' : 'custom-light'}`}>
-      <h2 className={`text-xl font-bold mb-2 ${darkMode ? 'text-gray-100' : 'text-gray-800'}`}>Summary:</h2>
-      <p className={`p-4 rounded-md ${darkMode ? 'bg-gray-700 text-gray-200' : 'bg-gray-100 text-gray-800'}`}>{summary}</p>
+      
+      <p className={`p-4 rounded-md ${darkMode ? 'bg-gray-700 text-gray-200' : 'bg-gray-100 text-gray-800'}`}>
+        <h2 className={`text-xl font-bold mb-2 ${darkMode ? 'text-gray-100' : 'text-gray-800'}`}>Summary:</h2>
+        {summary}
+        </p>
     </div>
   );
 }
